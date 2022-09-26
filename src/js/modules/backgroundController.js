1a2087b84901657b8e45bb2d26d1ad5b74ae1ebe
Welton Nogueira <welton.nogueira.dev@gmail.com>
2022-09-26 15:31:58 -0300

    Contact Bg Fix

--- src/js/modules/backgroundController.js
+++ src/js/modules/backgroundController.js
@@ -3,9 +3,11 @@ export const backgroundController = (data) => {
 
   const homeContainer = document.querySelector("#home .container");
   const homeContainerInfo = homeContainer.querySelector("h1");
+  const contactContainer = document.querySelector("#contact .container");
 
   const imagesBackground = data.filter(i => i.richTitle.length > 0);
-
+  
+  contactContainer.style.backgroundImage = `url(/assets/images/img_${imagesBackground[0].id}.jpg)`;
   const setData = (i) => {
     homeContainer.style.backgroundImage = `url(/assets/images/img_${imagesBackground[i].id}.jpg)`;
     homeContainerInfo.innerHTML = imagesBackground[i].richTitle;
